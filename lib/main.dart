6efecfde2e96fdcbb822c7970faf05afb9c350b2
yoshihiro-ri <ri.dev1027@gmail.com>
2024-07-27 12:05:10 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:qiita_search/screens/search_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Qiita Search',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        fontFamily: 'Hiragino Sans',
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF55C500),
+        ),
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.white,
+            ),
+      ),
+      home: const SearchScreen(),
+    );
+  }
+}
